2d3652b39d525bf1a0ba6e7e71c9d1b151327099
FarinFukunaga <65742085+FarinFukunaga@users.noreply.github.com>
2021-05-17 21:14:04 -0700

    Added helpers to core

---
+++ core/awshelpers.py
@@ -0,0 +1,19 @@
+import boto3
+import base64
+
+#Get secrets from secrets manager
+def get_secret_value(name, version=None):
+    secrets_client = boto3.client("secretsmanager")
+    kwargs = {'SecretId': name}
+    if version is not None:
+        kwargs['VersionStage'] = version
+    response = secrets_client.get_secret_value(**kwargs)
+    secret_string = response['SecretString']
+    secret_value_unstripped = secret_string.split(':')[1]
+    secret_value = re.sub('"|}','',secret_value_unstripped)
+
+    return secret_value
+
+def write_to_s3(s3_bucket, file_path, file_name):
+	s3 = boto3.resource('s3')   
+	s3.Bucket(s3_bucket).upload_file(file_path,file_name)
