9bb8bd8ef31bfa4c5a81b4c46e38ac9545b08da3
FarinFukunaga <65742085+FarinFukunaga@users.noreply.github.com>
2021-07-07 22:51:48 -0700

    Update quandl_call.py
    Added icecream for debugging

--- core/quandl_call.py
+++ core/quandl_call.py
@@ -4,6 +4,7 @@ import json
 import csv
 import pytz
 import re
+import icecream as ic
 
 import awshelpers
 
@@ -38,34 +39,15 @@ def request_quandl_api():
     final_url = f'https://www.quandl.com/api/v3/datasets/{database_code}/{dataset_code}/data.{return_format}?collapse={collapse}&start_date={start_date}&api_key={api_key_secret}'
     
     return requests.get(final_url)
+    ic()
 
 def response_to_local_csv(response,csv_path):
     
     f = open(csv_path, "w")
     f.write(response.text)
     f.close()
-    print('Wrote to local csv')
-
-    # #Call API
-    # response = requests.get(final_url)
-    # print(response.status_code)
-    # print(response.url)
-
-    # #Convert Response to CSV
-    # directory = '/home/ec2-user/files/sea_test/'
-    # csv_name = currenttime+'_quandl_test.csv'
-    # csv_path = directory+csv_name
-
-    # f = open(csv_path, "w")
-    # f.write(response.text)
-    # f.close()
-    # print('Wrote to local csv')
-
-    # #Write to S3
-    # s3_bucket = 'farin-prod-test'
-    # s3_path = 'test/quandl/'
-    # awshelpers.write_to_s3(s3_bucket,csv_path,s3_path,csv_name)
-    # print('Wrote to S3')
+    ic()
 
 #Run Function
 quandl_call()
+ic()
